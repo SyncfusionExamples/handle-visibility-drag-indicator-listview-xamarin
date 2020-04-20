9796ef7b69eb162dad0d966ad0896cf06be470c0
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-20 17:19:15 +0530

    XAMARIN-34601 - Technical review completed.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/DragValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class DragValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var dragMode = (DragStartMode)value;
+            if (dragMode == DragStartMode.OnHold || dragMode == DragStartMode.OnDragIndicator)
+                return true;
+            else
+                return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/ViewModel/ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace ListViewXamarin
+{
+    public class ViewModel
+    {
+        #region Fields
+
+        private ObservableCollection<ToDoItem> toDoList;
+
+        #endregion
+
+        #region Constructor
+
+        public ViewModel()
+        {
+            this.GenerateSource();
+        }
+        #endregion
+
+        #region Property
+
+        public ObservableCollection<ToDoItem> ToDoList
+        {
+            get { return toDoList;  }
+            set { this.toDoList = value; }
+        }
+       #endregion
+
+        #region Method
+
+        public void GenerateSource()
+        {
+            ToDoListRepository todoRepository = new ToDoListRepository();
+            toDoList = todoRepository.GetToDoList();
+        }
+        #endregion
+    }
+}
